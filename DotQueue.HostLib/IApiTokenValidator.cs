f7fff7919d4fd66e9d30a59724d6a57a7b2d9af3
Samer <samer_control@yahoo.com>
2016-11-17 23:31:03 -0500

    WIP

--- DotQueue.HostLib/IApiTokenValidator.cs
+++ DotQueue.HostLib/IApiTokenValidator.cs
@@ -1,7 +1,27 @@
-﻿namespace DotQueue.HostLib
+﻿using System;
+
+namespace DotQueue.HostLib
 {
     public interface IApiTokenValidator
     {
         bool IsValidToken(string token);
+    }
+
+    public interface IExceptionLogger
+    {
+        void Log(DotQueueException exception);
+    }
+
+    public class DotQueueException
+    {
+        public QueueEventType EventType { get; set; }
+        public Exception Exception { get; set; }
+        public DateTime ExceptionUtcDateTime { get; set; }
+    }
+
+    public enum QueueEventType
+    {
+        SubscriberOffline,
+        MessageIsnull,
     }
 }
