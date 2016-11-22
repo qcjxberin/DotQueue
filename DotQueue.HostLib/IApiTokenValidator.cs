6609effeb5fa648230df59e9909d0e2a44a975bc
Samer <samer_control@yahoo.com>
2016-11-21 23:35:45 -0500

    first working persistence repository

--- DotQueue.HostLib/IApiTokenValidator.cs
+++ DotQueue.HostLib/IApiTokenValidator.cs
@@ -1,27 +1,7 @@
-﻿using System;
-
-namespace DotQueue.HostLib
+﻿namespace DotQueue.HostLib
 {
     public interface IApiTokenValidator
     {
         bool IsValidToken(string token);
-    }
-
-    public interface IExceptionLogger
-    {
-        void Log(DotQueueException exception);
-    }
-
-    public class DotQueueException
-    {
-        public QueueEventType EventType { get; set; }
-        public Exception Exception { get; set; }
-        public DateTime ExceptionUtcDateTime { get; set; }
-    }
-
-    public enum QueueEventType
-    {
-        SubscriberOffline,
-        MessageIsnull,
     }
 }
